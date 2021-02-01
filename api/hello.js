c749289c3c1c99be72dcb9a7178fe07baef8920a
Bryanoxx <contact@bryanparasote.fr>
2021-02-01 19:45:38 +0000

    Code complet du stater de base

---
+++ api/hello.js
@@ -0,0 +1,16 @@
+import {
+  successObj,
+  errorObj
+} from '../src/utils/'
+
+export default (req, res) => {
+  const name = req.query.name
+  if (name) {
+    res.json(successObj({
+      hello: name
+    }))
+  } else {
+    res.status(400)
+    res.json(errorObj('no_name'))
+  }
+}
---
+++ src/utils.js
@@ -0,0 +1,27 @@
+/**
+ * Formattage de l'objet à envoyer en cas de succès
+ *
+ * @export
+ * @param {*} data
+ * @return {object}
+ */
+export function successObj (data) {
+  return {
+    status: 'success',
+    data
+  }
+}
+
+/**
+ * Formattage de l'objet à envoyer en cas d'échec
+ *
+ * @export
+ * @param {string} message
+ * @return {object}
+ */
+export function errorObj (message) {
+  return {
+    status: 'success',
+    message
+  }
+}
